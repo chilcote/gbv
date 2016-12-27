e12673009729f6bb4043608f12cbb88544bcc2f5
Joseph Chilcote <chilcote+github@gmail.com>
2016-12-27 14:46:32 -0800

    Stomapod Dispermous Monkeyshine Longhorn Aphyllous

--- gbv.py
+++ gbv.py
@@ -1,27 +1,37 @@
-#!/usr/bin/env python
-"""
-	Crack open a Milwaukee's Beast, grab some 
-	college rule from your peechee, and jot down
-	some song titles for the album of 26 songs that 
-	you will spend the next thirteen hours recording.
+#!/usr/bin/python
+'''
+    Crack open a Milwaukee's Beast, grab some
+    college rule from your peechee, and jot down
+    some song titles for the album of 26 songs that
+    you will spend the next thirteen hours recording.
 
-	Examples:
-		Adoptee Yengee Permixture Threadlet
-		Unplacable Cirri Whitebelly Shockable Antiabsolutist Stippen
-		Figured Coracine Mistranslate Physostegia
-		Seedman Tauroctonus Nonelementary Antadiform Eureka
-		Agatine Sparver
-"""
+    Examples:
+        Adoptee Yengee Permixture Threadlet
+        Unplacable Cirri Whitebelly Shockable Antiabsolutist Stippen
+        Figured Coracine Mistranslate Physostegia
+        Seedman Tauroctonus Nonelementary Antadiform Eureka
+        Agatine Sparver
+'''
 
-from random import choice
-from random import randint
+from random import choice, randint
 
-def songtitle(wordcount):
-	"""Generates a string of given length (wordcount), using Title Case."""
-	title = choice(words).title().strip()
-	for i in range(1, wordcount):
-		title = title + " " + choice(words).title().strip()
-	print title
+def get_songtitle(wordcount, words):
+    '''Generates a string of given length (wordcount), using Title Case.'''
+    songtitle = []
+    for i in range(1, wordcount):
+        songtitle.append(choice(words).title().strip())
+    while True:
+        char_count = len(' '.join(songtitle))
+        if char_count <= 114:
+            break
+        songtitle.pop()
+    return ' '.join(songtitle)
 
-words = open("/usr/share/dict/words", 'r').readlines()
-songtitle(randint(2, 7))
+def main():
+    with open('/usr/share/dict/words', 'r') as f:
+        words = f.readlines()
+    bob_sez = 'My favorite GBV song is %s!' % get_songtitle(randint(4, 17), words)
+    print bob_sez
+
+if __name__ == "__main__":
+    main()
